bfcbe02a2fb7b28b3e6f2eada817a3fbf8bad2c6
Mahesh Patel <mahesh303@hotmail.com>
2025-01-07 16:24:04 -0700

    Initial Commit

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Mahesh Patel on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gittestTests/gittestTests.swift
@@ -0,0 +1,17 @@
+//
+//  gittestTests.swift
+//  gittestTests
+//
+//  Created by Mahesh Patel on 1/7/25.
+//
+
+import Testing
+@testable import gittest
+
+struct gittestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
